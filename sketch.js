ff641a587dfdb9549801531f20cc7c1cfc1bf17d
BigTendyT <129558840+BigTendyT@users.noreply.github.com>
2023-04-19 08:42:58 -0500

    Updated Web Layout

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 
+
 let gameFrame;
 let url;
 let x = false;
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+    loadWeb();
+  }
+}
 
 function draw() {
   url=select('#URL');
@@ -9,9 +16,11 @@ function draw() {
   if (x==false){
     rect(0,0,window.innerWidth, window.innerHeight);
   textSize(50);
-  text("Blocked Site Loader.",30,70);
+  x1=innerWidth/2-textWidth("Blocked Site Loader.")/2;
+  text("Blocked Site Loader.",x1,70);
   textSize(22);
-  text("Sites that return the error: (Website) refused to load, DO NOT work using this method.",30,110);
+    x1=innerWidth/2-textWidth("Sites that return the error: (Website) refused to load/connect, DO NOT work using this method.")/2;
+  text("Sites that return the error: (Website) refused to load/connect, DO NOT work using this method.",x1,110);
   }
 }
 
@@ -20,7 +29,7 @@ function loadWeb(){
   console.log(url2);
   gameFrame = createDiv();
   gameFrame.position(0, 50);
-  gameFrame.size(window.innerWidth, window.innerHeight);
+ gameFrame.size(window.innerWidth, window.innerHeight);
   
   gameFrame.html(`
     <iframe 
@@ -30,8 +39,13 @@ function loadWeb(){
       allow="autoplay; fullscreen; pointer-lock" 
       frameborder="0"
       sandbox="allow-scripts allow-same-origin allow-pointer-lock"
+  
     ></iframe>
   `);
+}
+
+function clearValue(){
+  url.value('');
 }
 
 function setup() {
@@ -39,9 +53,9 @@ function setup() {
   let button = select('#conf');
   button.mousePressed(loadWeb);
   
+  let button2 = select('#clearLink');
+  button2.mousePressed(clearValue);
+  
   
 }
-
-
-
 
